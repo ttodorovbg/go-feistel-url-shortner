de03a54432d77a32f75bc47f43be0daffdfc3d5a
Todor Todorov <tt_555bg@yahoo.com>
2026-05-03 16:49:08 +0300

    test hooks

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/ttodorovbg/go-feistel-url-shortener/internal/cli"
@@ -13,7 +12,7 @@ func main() {
 	if err != nil {
 		// log.Fatal(err)
 		fmt.Fprintln(os.Stderr, err)
-		-os.Exit(1)		+
+		os.Exit(1)
 	}
 
 	fmt.Println(result)
